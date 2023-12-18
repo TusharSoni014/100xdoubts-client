8396c94fb2cf13974f8921b7121ed5cea2987368
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-18 16:09:55 +0530

    added login function and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,47 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { handleCatch } from "../utils/utilFunctions";
+import { CiLogout } from "react-icons/ci";
+import axiosClient from "../utils/axiosClient";
+import { updateLoginStatus, updateUser } from "../redux/slices/appSlice";
 
 export default function Header() {
+  const isLoggedIn = useSelector((state) => state.appSlice.isLoggedIn);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSignout = async (e) => {
+    try {
+      await axiosClient.post("/user/logout");
+      dispatch(updateUser({}));
+      dispatch(updateLoginStatus(false));
+    } catch (error) {
+      handleCatch(error);
+    }
+  };
+
   return (
     <nav className="__header h-[60px] bg-gray-900 text-white p-3 flex justify-between items-center">
       <h3 className="font-bold">100xDoubts</h3>
       <div className="__btn_container">
-        <button
-          onClick={() => navigate("/login")}
-          className="bg-blue-500 transition hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Sign In
-        </button>
+        {isLoggedIn ? (
+          <>
+            <button
+              onClick={handleSignout}
+              className="bg-red-500 transition hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              <CiLogout />
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-blue-500 transition hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Sign In
+          </button>
+        )}
       </div>
     </nav>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
-import { handleGoogleLogin } from "../utils/utilFunctions";
+import { handleCatch, handleGoogleLogin } from "../utils/utilFunctions";
 import { useNavigate } from "react-router-dom";
+import axiosClient from "../utils/axiosClient";
+import { useDispatch } from "react-redux";
+import { updateLoginStatus, updateUser } from "../redux/slices/appSlice";
+import { toast } from "react-toastify";
 
 export default function Login() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosClient.post("/user/login", {
+        username: email,
+        password: password,
+      });
+      dispatch(updateUser(response.data.user));
+      dispatch(updateLoginStatus(true));
+      toast.success("Logged in successfully!");
+      navigate("/");
+    } catch (error) {
+      handleCatch(error);
+    }
+  };
   return (
     <div className="__login_page h-[calc(100dvh-60px)] w-full flex justify-center items-center">
       <div className="__login_form_container p-5 rounded bg-gray-800 w-[300px]">
-        <form className="flex gap-3 flex-col">
+        <form onSubmit={handleLogin} className="flex gap-3 flex-col">
           <input
             className="p-2 rounded outline-none text-black"
             type="text"
             placeholder="Enter Email or Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
+            disabled={loading}
           />
           <input
             className="p-2 rounded outline-none text-black"
             type="password"
             placeholder="Enter Password"
             required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={loading}
           />
           <button
             className="bg-blue-500 w-full transition hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
