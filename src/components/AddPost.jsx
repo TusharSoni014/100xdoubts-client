69eb19f8084f79534222b6eb8b8a511c975813bb
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-18 18:41:22 +0530

    added create page

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import { handleCatch } from "../utils/utilFunctions";
 
 export default function AddPost() {
   const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
 
   const modules = {
     toolbar: [
@@ -21,13 +23,27 @@ export default function AddPost() {
     ],
   };
 
+  const handlePostDoubt = async (e) => {
+    e.preventDefault();
+    try {
+      console.log({ title: title, description: description });
+    } catch (error) {
+      handleCatch(error);
+    }
+  };
   return (
-    <div className="__add_post_container border-2 border-red-500 p-3">
-      <form onSubmit={handlePostDoubt} className="flex flex-col gap-2">
+    <div className="__add_post_container p-3">
+      <h1 className="text-4xl my-3">Post a new Doubt</h1>
+      <form
+        onSubmit={handlePostDoubt}
+        className="flex flex-col gap-2 px-8 my-5"
+      >
         <input
           className="w-full outline-none p-2 rounded text-white bg-gray-600"
           type="text"
           placeholder="Title of the Doubt"
+          required
+          onChange={(e) => setTitle(e.target.value)}
         />
         <ReactQuill
           theme="bubble"
@@ -37,12 +53,15 @@ export default function AddPost() {
           placeholder="Type the description of the doubt here."
           modules={modules}
         />
-        <div className="__img_previews">[Images will preview here]</div>
+        {/* <div className="__img_previews">[Images will preview here]</div> */}
         <div className="__btn_container flex justify-start items-center gap-2">
-          <button className="bg-green-500 transition hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          {/* <button className="bg-green-500 transition hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             + Add Image
-          </button>
-          <button className="bg-blue-500 transition hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          </button> */}
+          <button
+            type="submit"
+            className="bg-blue-500 transition hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
             Post Doubt
           </button>
         </div>
